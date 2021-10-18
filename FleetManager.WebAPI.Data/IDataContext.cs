a9653258e17ec58de5e3e096ce9414d0ddb4f156
Josefine Paaske <1086237@ucn.dk>
2021-10-18 16:32:44 +0200

    Class libraries

--- FleetManager.WebAPI.Data/IDataContext.cs
+++ FleetManager.WebAPI.Data/IDataContext.cs
@@ -5,6 +5,7 @@ namespace FleetManager.WebAPI.Data
 {
     public interface IDataContext
     {
+        //Skal stå tomt. Det er en type, der snakker med data context 
 
     }
  
@@ -12,13 +13,17 @@ namespace FleetManager.WebAPI.Data
     /// Interface for generic connections to a datasource
     /// </summary>
     /// <typeparam name="TConnection">Type of connection (e.g., IDbConnection for a database connection)</typeparam>
-    public interface IDataContext<TConnection> : IDataContext
+    public interface IDataContext<TConnection> : IDataContext //Generisk IDataContext
     {
         TConnection Open();
     }
 
     public interface ITypedDataContext : IDataContext
     {
+        /**
+         * ITypedData er en collection. Her er en Cars Collection og en Location Collection
+         * Begge interfaces arver fra IDataContext, så man kan lave instancer af begge på en gang
+         */
         IEnumerable<Car> Cars { get; }
         IEnumerable<Location> Locations { get; }
 
