73886501af2c586d6e70813144a125223bb89dd6
Josefine Paaske <1086237@ucn.dk>
2021-10-18 16:32:25 +0200

    Dokumentation DaoFactory og Else if er lavet

--- FleetManager.WebAPI.Data/DaoFactory.cs
+++ FleetManager.WebAPI.Data/DaoFactory.cs
@@ -1,22 +1,32 @@
 ﻿using FleetManager.WebAPI.Model;
 using System;
+using System.Data;
 
 namespace FleetManager.WebAPI.Data
 {
     public static class DaoFactory
     {
-        public static IDao<TModel> Create<TModel>(IDataContext dataContext)
+        public static IDao<TModel> Create<TModel>(IDataContext dataContext) //IDataContext er polymorfi her
         {
-            Type dataContextType = dataContext.GetType();
+            Type dataContextType = dataContext.GetType(); //Typen trækkes ud med GetType()
 
             // TODO: (Step 3) add check for the sql server datacontext interface type and return the correct dao
 
-            if (typeof(ITypedDataContext).IsAssignableFrom(dataContextType))
+            if (typeof(ITypedDataContext).IsAssignableFrom(dataContextType)) //Er dette sandt, så køres blokken af koden under
             {
                 return typeof(TModel) switch
                 {
                     var dao when dao == typeof(Car) => new Daos.Memory.CarDao(dataContext as ITypedDataContext) as IDao<TModel>,
                     var dao when dao == typeof(Location) => new Daos.Memory.LocationDao(dataContext as ITypedDataContext) as IDao<TModel>,
+                    _ => throw new DaoFactoryException($"Model [{typeof(TModel).Name}] not supported"),
+                };
+            }
+            else if (typeof(IDataContext<IDbConnection>).IsAssignableFrom(dataContextType))
+            {
+                return typeof(TModel) switch
+                {
+                    var dao when dao == typeof(Car) => new Daos.SQL.CarDao(dataContext as IDataContext<IDbConnection>) as IDao<TModel>,
+                    var dao when dao == typeof(Location) => new Daos.SQL.LocationDao(dataContext as IDataContext<IDbConnection>) as IDao<TModel>,
                     _ => throw new DaoFactoryException($"Model [{typeof(TModel).Name}] not supported"),
                 };
             }
